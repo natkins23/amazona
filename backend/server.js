50db922a9a68d6eed3a7bde27b911660d34bdd39
Nathan Watkins <nathancwatkins23@gmail.com>
2022-09-23 18:24:07 -0700

    update server js for paypal

--- backend/server.js
+++ backend/server.js
@@ -19,6 +19,10 @@ const app = express()
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
+app.get('/api/keys/paypal', (req, res) => {
+    res.send(process.env.PAYPAL_CLIENT_ID || 'sb')
+})
+
 app.use('/api/seed', seedRouter)
 app.use('/api/products', productRouter)
 app.use('/api/users', userRouter)
@@ -28,9 +32,9 @@ const port = process.env.port || 5000
 
 const __dirname = path.resolve()
 app.use(express.static(path.join(__dirname, '/frontend/build')))
-app.get('*', (req, res) => {
+app.get('*', (req, res) =>
     res.sendFile(path.join(__dirname, '/frontend/build/index.html'))
-})
+)
 
 app.use((err, req, res, next) => {
     res.status(500).send({ message: err.message })
